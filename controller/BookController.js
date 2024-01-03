9d627bd1aeba61f3dde87085d9b3b4deaa6e888f
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-01-03 11:30:14 +0900

    [Refactor] -book controller to retrieve data from the database

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,11 +2,31 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-  res.json("전체 도서 조회");
+  let sql = "SELECT * FROM books";
+  conn.query(sql, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+    return res.status(StatusCodes.OK).json(results);
+  });
 };
 
 const bookDetail = (req, res) => {
-  res.json("개별 도서 조회");
+  let { id } = req.params;
+  let sql = "SELECT * FROM books WHERE id = ?";
+  conn.query(sql, id, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+
+    if (results[0]) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.NOT_FOUND).end();
+    } // NOT FOUND
+  });
 };
 
 const bookByCategory = (req, res) => {
