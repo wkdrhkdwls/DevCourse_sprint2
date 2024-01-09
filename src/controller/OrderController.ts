f0bf9cb24425153467f5ebc8ab88e00091638aa5
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-01-09 13:14:01 +0900

    [Feat] - Order 컨트롤러 생성

---
+++ src/controller/OrderController.ts
@@ -0,0 +1,55 @@
+import conn from "@database/mariadb";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+import { RowDataPacket } from "mysql2";
+
+const order = (req: Request, res: Response): void => {
+  const { book_id, quantity, user_id } = req.body;
+  let sql =
+    "INSERT INTO cartitems (book_id, quantity, user_id) VALUES (?, ?, ?)";
+  let values = [book_id, quantity, user_id];
+  conn.query(sql, values, (err: Error, results: RowDataPacket[]) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+      return;
+    }
+
+    res.status(StatusCodes.OK).json(results);
+  });
+};
+
+const getOrders = (req: Request, res: Response): void => {
+  const { user_id, selected } = req.body;
+  let sql =
+    "SELECT cartitems.id, book_id, title, summary, quantity, price FROM cartitems LEFT JOIN books ON cartitems.book_id = books.id WHERE user_id=? AND cartitems.id IN (?)";
+  let values = [user_id, selected];
+  conn.query(sql, values, (err: Error, results: RowDataPacket[]) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+      return;
+    }
+
+    res.status(StatusCodes.OK).json(results);
+  });
+};
+
+const getOrderDetail = (req: Request, res: Response): void => {
+  const { id } = req.params;
+
+  let sql = "DELETE FROM carts WHERE id = ?";
+
+  conn.query(sql, id, (err: Error, results: RowDataPacket[]) => {
+    if (err) {
+      console.log(err);
+      res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+      return;
+    }
+
+    res.status(StatusCodes.OK).json(results);
+  });
+};
+
+export { order, getOrders, getOrderDetail };
--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,19 +1,14 @@
+import { order, getOrders, getOrderDetail } from "@controller/OrderController";
 import express from "express";
 const router = express.Router();
 
 router.use(express.json());
 
 // 결제하기 = 주문하기 = 주문 등록 = 데이터베이스 주문 INSERT
-router.post("/", (req, res) => {
-  res.json("결제하기");
-});
+router.post("/", order);
 // 주문 내역(목록) 조회
-router.get(`/`, (req, res) => {
-  res.json("주문 내역(목록) 조회");
-});
+router.get(`/`, getOrders);
 // 주문 상세 상품 조회
-router.get(`/:id`, (req, res) => {
-  res.json("주문 상세 상품 조회");
-});
+router.get(`/:id`, getOrderDetail);
 
 export default router;
