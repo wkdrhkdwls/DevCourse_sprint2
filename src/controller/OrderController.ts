455d2b9057afd633eae6871b44817d5d04f5f61f
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2024-01-10 12:39:00 +0900

    Refactor order creation process

--- src/controller/OrderController.ts
+++ src/controller/OrderController.ts
@@ -5,15 +5,55 @@ import { StatusCodes } from "http-status-codes";
 import { RowDataPacket } from "mysql2";
 
 const order = (req: Request, res: Response): void => {
-  const { book_id, quantity, user_id } = req.body;
-  let sql =
-    "INSERT INTO cartitems (book_id, quantity, user_id) VALUES (?, ?, ?)";
-  let values = [book_id, quantity, user_id];
+  const { items, delivery, totalQuantity, totalPrice, userId, firstBookTitle } =
+    req.body;
+
+  let delivery_id;
+  let order_id;
+
+  let sql = `INSERT INTO delivery (address, receiver, contact)
+     VALUES (?, ?, ?)`;
+  let values = [delivery.address, delivery.receiver, delivery.contact];
+
   conn.query(sql, values, (err: Error, results: RowDataPacket[]) => {
     if (err) {
       console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+
+    // @ts-ignore
+    delivery_id = results.insertId;
+    // @ts-ignore
+    console.log(results.insertId);
+
+    res.status(StatusCodes.OK).json(results);
+  });
+  sql = `INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) 
+          VALUES (?,?,?,?,?)`;
+  values = [firstBookTitle, totalQuantity, totalPrice, userId, delivery_id];
+  conn.query(sql, values, (err: Error, results: RowDataPacket[]) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+    }
+
+    // @ts-ignore
+    order_id = results.insertId;
+
+    res.status(StatusCodes.OK).json(results);
+  });
+
+  sql = `INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?`;
+  values = [];
+
+  items.forEach((item: any) => {
+    values.push([order_id, item.book_id, item.quantity]);
+    console.log(values);
+  });
+  conn.query(sql, [values], (err: Error, results: RowDataPacket[]) => {
+    if (err) {
+      console.log(err);
       res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
-      return;
     }
 
     res.status(StatusCodes.OK).json(results);
